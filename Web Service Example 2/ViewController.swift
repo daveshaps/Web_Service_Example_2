3ca1028d6d4cee263690a6e8c5673cf44e934a4d
Wish Carr <wishcarr@wishs-macbook-pro.local>
2016-11-23 12:30:38 -0500

    Initial Commit

---
+++ Web Service Example 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Web Service Example 2
+//
+//  Created by Wish Carr on 11/23/16.
+//  Copyright © 2016 David Shapiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
